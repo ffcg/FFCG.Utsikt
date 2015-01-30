57c3b6d9aecede765291fba52f342096b17dd280
Jesper Brännström <jesper.brannstrom@ffcg.se>
2015-01-30 19:58:50 +0100

    Added basic service to the controller that retrieves newsfeed

--- FFCG.Utsikt.Web/ClientApp/app.js
+++ FFCG.Utsikt.Web/ClientApp/app.js
@@ -1,5 +1,38 @@
 ﻿var myApp = angular.module('epiApp', []);
 
-myApp.controller('homeController', ['$scope', function ($scope) {
-    $scope.data = "Epi!";
+myApp.controller('homeController', ['$scope', 'dataService', function ($scope, dataService) {
+
+    $scope.news = [];
+
+    dataService.getData()
+    .then(function (data) {
+        angular.copy(data, $scope.news);
+    },
+        function () {
+            //Handle error here
+        })
+    .then(function () {
+        //Afterwards
+    });
 }]);
+
+myApp.factory('dataService', ['$q', '$http',
+    function ($q, $http) {
+        var _service = {}
+
+        _service.getData = function() {
+            var deferred = $q.defer();
+
+            $http.get('newsfeed')
+                .then(function (result) {
+                    deferred.resolve(result.data);
+                }, function () {
+                    deferred.reject();
+                });
+
+            return deferred.promise;
+        }
+
+        return _service;
+    }
+]);
