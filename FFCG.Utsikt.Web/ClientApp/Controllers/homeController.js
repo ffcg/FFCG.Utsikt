98deed90830fce0f3d3b3e8302dd679e13033899
viktor <viktordoth@gmail.com>
2015-01-30 23:06:08 +0100

    refactored angular app.

---
+++ FFCG.Utsikt.Web/ClientApp/Controllers/homeController.js
@@ -0,0 +1,23 @@
+﻿myApp.controller('homeController', ['$scope', 'dataService', function ($scope, dataService) {
+    $scope.isBusy = true;
+    $scope.news = [];
+
+    $scope.showText = function (article) {
+        if (!article.showArticle) {
+            article.showArticle = true;
+        } else {
+            article.showArticle = false;
+        }
+    }
+
+    dataService.getData()
+    .then(function (data) {
+        angular.copy(data, $scope.news);
+    },
+        function () {
+            //Handle error here
+        })
+    .then(function () {
+        $scope.isBusy = false;
+    });
+}]);
