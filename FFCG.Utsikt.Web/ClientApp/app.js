4b824af31f0d6fc0f6f7c73c6fab3b1b8c026047
Daniel Gustafsson <gustafsson@Daniels-MacBook-Pro-2.local>
2015-01-31 09:37:34 +0100

    HTML bind
    Fixed html tags in Preamble.

--- FFCG.Utsikt.Web/ClientApp/app.js
+++ FFCG.Utsikt.Web/ClientApp/app.js
@@ -2,7 +2,6 @@
 
 angular.module('services', []);
 
-myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-
+myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     // set up stateProvider with states n stuff
 }]);
